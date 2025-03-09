9cf1b4631f514eb9c1dc2a49f422b4c4bd99c6d2
uwethuemmel <uwethuemmel@users.noreply.github.com>
2025-03-09 15:53:17 +0000

    deploy: 0cb5ae919200f5a562e5ee707927ead60daefc0f

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Helsinki",
+        "excerpt":"Overview  Helsinki’s data strategy aims to create an open and transparent city by making data accessible and encouraging the development of smart city solutions. Open data platforms and public-private partnerships are key components of this strategy.   ","categories": [],
+        "tags": [],
+        "url": "/dataandthecity/cities/helsinki.html",
+        "teaser": null
+      },{
+        "title": "New_york",
+        "excerpt":"Overview  New York’s data strategy revolves around improving public services through open data initiatives and encouraging civic engagement.   ","categories": [],
+        "tags": [],
+        "url": "/dataandthecity/cities/new_york.html",
+        "teaser": null
+      },{
+        "title": "Zurich",
+        "excerpt":"Overview  Zurich has implemented a comprehensive data strategy aimed at enhancing city services, improving governance, and fostering transparency. The city’s open data platform serves as the foundation for building innovative public services.   ","categories": [],
+        "tags": [],
+        "url": "/dataandthecity/cities/zurich.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/dataandthecity/jekyll/update/2025/03/08/welcome-to-jekyll.html",
+        "teaser": null
+      }]
